4caa7a1c3490b19859619bac17ffc8b6e609fa19
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-11 16:05:05 -0400

    Add person class and method speak()

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,24 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+        this.gender = props.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I'm from ${this.location}`;
+    }
+}
+
 // comments to commit
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });

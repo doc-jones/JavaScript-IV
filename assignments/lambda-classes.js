469eff462a040c7bdae73187f6d563ad5d7046e8
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-11 16:16:55 -0400

    Add Instructor class and methods demo, grade

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,6 +11,24 @@ class Person {
     }
 }
 
+class Instructor extends Person{
+    constructor(props) {
+        super(props);
+        this.specialty = props.specialty;
+        this.favLanguage = props.favLanguage;
+        this.catchPhrase = props.catchPhrase;
+    }
+    demo(subject) {
+        return 'Today we are learning about {subject}';
+    }
+    grade(Student, subject) {
+        return '{student.name} receives a perfect score on {subject}';
+    }
+}
+
+console.log(Instructor.demo());
+console.log(Instructor.grade());
+
 // comments to commit
 
 const fred = new Instructor({
